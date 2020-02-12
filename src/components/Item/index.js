aac41fccf1bb09b05b123d47fc7001b16eb7ff44
Stephanie Fernandes <steph3001@gmail.com>
2020-02-11 22:45:55 -0300

    Create Item component

---
+++ src/components/Item/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+
+const Item = () => {
+  return (
+    <div className='item'>
+      <div className='item-left'>
+        <div className='item-left__circle'>
+          <span className='initial'>R</span>
+        </div>
+      </div>
+      <div className='item-body'>
+        <div className='item-body__top'>
+          <div className='sender'>Remetente</div>
+          <div className='icons'></div>
+        </div>
+        <div className='item-body__middle'>
+          <div className='title'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quisquam assumenda dolor dolores nemo a consequatur ea blanditiis ut eaque reiciendis! Fugit debitis quaerat autem facilis veritatis numquam sed alias!</div>
+          <div className='date'>Jun 5/4</div>
+        </div>
+        <div className='item-body__bottom'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quisquam assumenda dolor dolores nemo a consequatur ea blanditiis ut eaque reiciendis! Fugit debitis quaerat autem facilis veritatis numquam sed alias!
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Item
