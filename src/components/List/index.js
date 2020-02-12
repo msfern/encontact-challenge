21560c4b7b9a3e4a7c537733bcd73e78f796f240
Stephanie Fernandes <steph3001@gmail.com>
2020-02-11 22:45:43 -0300

    Create List component

---
+++ src/components/List/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Item from '../Item/index';
+import './style.css';
+
+const List = () => {
+  return (
+    <div className='list'>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+    </div>
+  )
+}
+
+export default List
